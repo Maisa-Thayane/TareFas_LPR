b8ee746397dfec75712afcad5bd9e7e88f447a6f
Maisa-Thayane <isa14092007@gmail.com>
2025-06-30 11:28:22 -0300

    Add files via upload

---
+++ Aula9/EX1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        List<string> nomes = new();
+
+        Console.WriteLine("Digite o tamanho da lista: ");
+        int tamanho = int.Parse(Console.ReadLine());
+        for (int i = 0; i < tamanho; i++)
+        {
+            Console.WriteLine("Digite o nome: ");
+            string n = Console.ReadLine();
+            nomes.Add(n);
+        }
+        while
+        for (int i = 0; i < nomes.Count; i++)
+        {
+            Console.WriteLine(nomes[i]);
+        }
+    }
+}
---
+++ Aula9/exemplo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+class Exemplo
+{
+    static void Main()
+    {
+        int superiores = 0;
+        int repeticoes = 0;
+        List<int> numeros = new List<int> { 1, 2, 3, 4, 5 };
+
+        numeros.Add(4);
+        numeros.Insert(2, 7);
+
+        for (int i = 0; i < numeros.Count; i++)
+        {
+            if (numeros[i] == 8)
+            {
+                Console.WriteLine("Sim, o numero 8 foi encontrado.");
+            }
+        }
+        for (int i = 0; i < numeros.Count; i++)
+        {
+            while (numeros[i] > 4)
+            {
+                superiores++;
+            }
+        }
+        Console.WriteLine("A quantidade de numeros maiores que 4: ");
+        Console.WriteLine(superiores);
+
+        for (int i = 0; i < numeros.Count; i++)
+        {
+            while (numeros[i] == 3)
+            {
+                repeticoes++;
+            }
+        }
+        Console.WriteLine("A quantidade de vezes em que 3 apareceu: ");
+        Console.WriteLine(repeticoes);
+        for(int i = 0; i < numeros.Count; i++){
+        while (numeros[i] == 4)
+        {
+            numeros.Remove(4);
+        }
+        }
+        numeros.Remove(2);
+    }
+}
