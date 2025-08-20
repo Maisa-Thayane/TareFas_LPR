ac4350ee0b8fadb6c7e935f8dfbd9a5e656b5629
Maisa Thayane dos Santos <isa14092007@gmail.com>
2025-08-20 07:19:43 -0300

    Update Comentando

--- Aula7/ex4/ex4.cpp
+++ Aula7/ex4/ex4.cpp
@@ -2,41 +2,44 @@
 using namespace std;
 
 int main() {
+    // Declaração das matrizes A, B e C (3x3)
     int A[3][3];
     int B[3][3];
-    int C[3][3] = {0}; 
+    int C[3][3] = {0}; // Inicializa todos os elementos com zero
 
-    
+    // Entrada dos elementos da matriz A
     cout << "Digite os elementos da matriz A (3x3):\n";
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cin >> A[i][j];
+    for (int i = 0; i < 3; i++) {        // Percorre as linhas
+        for (int j = 0; j < 3; j++) {    // Percorre as colunas
+            cin >> A[i][j];              // Lê o valor digitado pelo usuário
         }
     }
 
-    
+    // Entrada dos elementos da matriz B
     cout << "Digite os elementos da matriz B (3x3):\n";
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cin >> B[i][j];
+    for (int i = 0; i < 3; i++) {        // Percorre as linhas
+        for (int j = 0; j < 3; j++) {    // Percorre as colunas
+            cin >> B[i][j];              // Lê o valor digitado pelo usuário
         }
     }
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            for (int k = 0; k < 3; k++) {
-                C[i][j] += A[i][k] * B[k][j];
+    // Multiplicação das matrizes A x B
+    for (int i = 0; i < 3; i++) {           // Percorre as linhas da matriz resultante C
+        for (int j = 0; j < 3; j++) {       // Percorre as colunas da matriz C
+            for (int k = 0; k < 3; k++) {   // Percorre a linha de A e coluna de B
+                C[i][j] += A[i][k] * B[k][j]; // Soma do produto dos elementos correspondentes
             }
         }
     }
 
+    // Exibe a matriz resultante C
     cout << "Resultado da multiplicação das matrizes A x B:\n";
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << C[i][j] << " ";
+    for (int i = 0; i < 3; i++) {        // Percorre as linhas
+        for (int j = 0; j < 3; j++) {    // Percorre as colunas
+            cout << C[i][j] << " ";      // Mostra cada elemento seguido de espaço
         }
-        cout << endl;
+        cout << endl;                    // Quebra de linha após cada linha da matriz
     }
 
-    return 0;
+    return 0; // Encerra o programa
 }
